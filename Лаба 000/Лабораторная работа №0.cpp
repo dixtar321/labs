e79228deb167f46df17418eb006738030ba17f84
pogosoffal <78268250+dixtar321@users.noreply.github.com>
2021-09-17 00:29:55 +0300

    added "second" and "third"

--- Лаба 000/Лабораторная работа №0.cpp
+++ Лаба 000/Лабораторная работа №0.cpp
@@ -4,9 +4,9 @@ int main() {
 	int a, b, c, res; //Программа (ЛР №0) высчитывает разность между 2-ым и 3-им числами
 	printf("Enter the first number: ");
 	scanf("%d", &a);
-	printf("Enter the first number: ");
+	printf("Enter the second number: ");
 	scanf("%d", &b);
-	printf("Enter the first number: ");
+	printf("Enter the third number: ");
 	scanf("%d", &c);
 	res = b - c;
 	printf("Result: %d - %d = %d", b, c, res);
