da84add9cc1e088efdfdd22a0305929a4edb0ff2
pogosoffal <78268250+dixtar321@users.noreply.github.com>
2021-09-24 18:48:32 +0300

    Added lab1 and dop1

---
+++ Source.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() { //Ввести N натуральных чисел. Завершить ввод 0-м. Вывести максимальное число, минимальное число и их номера.
+	int rs=5, IndexMax=0, IndexMin=0;
+	int N[5], Max = 0, Min = 1000, i, input;
+
+	printf("Enter 5 numbers\n");
+	scanf_s("%d %d %d %d %d", &N[0], &N[1], &N[2], &N[3], &N[4]);
+	for (i = 0; i < rs; i++) {
+		
+		if (N[i] > Max) {
+			Max = N[i];
+			IndexMax = i;
+		}
+		if (N[i] < Min) {
+			Min = N[i];
+			IndexMin = i;
+		}
+	}
+	printf("Largest number: %d - %d\n", Max, IndexMax);
+	printf("Minimal number: %d - %d", Min, IndexMin);
+	return 0;
+}
